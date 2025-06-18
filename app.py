7bb18b1a3bde9f2b32c936372ed57111d4a827ca
hannywahyu <haniwahyu17@students.unnes.ac.id>
2025-06-18 10:34:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,18 +11,6 @@ from sklearn.metrics import (
 )
 from sklearn.preprocessing import label_binarize
 
-from sklearn.model_selection import train_test_split
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.2, random_state=42)
-
-model.fit(X_train, y_train)  # hanya latih di data training
-y_pred = model.predict(X_test)  # evaluasi hanya di test set
-
-from sklearn.model_selection import cross_val_score
-
-scores = cross_val_score(model, X, y, cv=5, scoring='accuracy')
-print(f"Mean CV Accuracy: {scores.mean():.4f}")
-
 # ===========================
 # 1. Load Model & Scaler
 # ===========================
